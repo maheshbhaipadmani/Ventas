86b069279fa99105af6f61ed8ff58b0cbe428728
patel-hitixa <hitixa.bhuva@theuniqueitsolution.com>
2025-02-19 17:26:18 +0530

    change

--- assets/A_Layout/Footer/Footer_Partials/A_FooterMain.php
+++ assets/A_Layout/Footer/Footer_Partials/A_FooterMain.php
@@ -56,9 +56,9 @@ echo
 
 <div class="contact-info">
   <span class="contact-icon">
-    <a href="mailto:info@theventas.comm" class="black-link">
+    <a href="mailto:info@theventas.com" class="black-link">
       <i class="fa fa-envelope" aria-hidden="true"></i>
-      info@theventas.comm
+      info@theventas.com
     </a>
   </span>
 </div>
